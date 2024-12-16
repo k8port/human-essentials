d18c9a25e8ef575fd600c2d1d29ea271b0928cb5
k8port <k8portalatin@gmail.com>
2024-12-16 16:39:24 -0600

    Revert "lowered pagination threshold in dev and staging"
    This reverts commit c2e211aed2e497d37c92aaf28b6ce451d2488b1b.

--- config/initializers/kaminari_config.rb
+++ config/initializers/kaminari_config.rb
@@ -1,10 +1,6 @@
 # frozen_string_literal: true
 Kaminari.configure do |config|
-  if Rails.env.development? || Rails.env.staging?
-    config.default_per_page = 5
-  else
-    config.default_per_page = 50
-  end
+  config.default_per_page = 50
   # config.max_per_page = nil
   # config.window = 4
   # config.outer_window = 0
